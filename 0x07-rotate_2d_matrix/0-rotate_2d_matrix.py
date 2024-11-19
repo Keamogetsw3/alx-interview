7e179350bba09a562c7e62d177fcb13acc8c86e7
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-11-19 20:40:32 +0200

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """
-n x n 2D matrix, rotate it 90 degrees clockwise
-param:
-    matrix: represents a row of the matrix
-
+n x n 2D matrix
 """
 
 
 def rotate_2d_matrix(matrix):
+    """
+    rotate n x n 2D matrix 90 degrees clockwise
+    param:
+        matrix: represents a row of the matrix
+    """
+    

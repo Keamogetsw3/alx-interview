3cb1cb31ed42b289504fee1c7712f5e4424775d7
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-11-27 12:52:42 +0200

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,7 +5,7 @@ Python program for coin change problem using recursion
 
 
 def makeChange(coins, total):
-    """ 
+    """
     This function returns the fewest number of.
     coins needed to make up the given total.
     If the total cannot be made up with the given coins, it returns -1.
@@ -15,7 +15,8 @@ def makeChange(coins, total):
     total: the total value we want to make with the fewest number of coins
 
     Returns:
-    int: the fewest number of coins needed to form the total, or -1 if it's not possible
+    int: the fewest number of coins needed to form the total,
+    or -1 if it's not possible
     """
     if total <= 0:
         return 0
@@ -31,7 +32,7 @@ def makeChange(coins, total):
                 count += 1
             elif value < newList[index]:
                 index += 1
-        
+
         if index == len(newList):
             if value != 0:
                 return -1

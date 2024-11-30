4b1a0170324b6c999a6b0e5c5beffc2a1980b8d5
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-12-01 00:52:31 +0200

    fixed: 0-minoperations.py:2:4: W291 trailing whitespace 0-minoperations.py:3:66: W291 trailing whitespace 0-minoperations.py:7:1: E302 expected 2 blank lines, found 1 0-minoperations.py:9:80: E501 line too long (94 > 79 characters) 0-minoperations.py:10:1: W293 blank line contains whitespace 0-minoperations.py:15:80: E501 line too long (85 > 79 characters) 0-minoperations.py:19:1: W293 blank line contains whitespace 0-minoperations.py:22:1: W293 blank line contains whitespace 0-minoperations.py:29:1: W293 blank line contains whitespace

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
-""" 
- method that calculates the fewest number of operations needed to 
+"""
+ method that calculates the fewest number of operations needed to
  result in exactly n H characters in the file.
 """
 
+
 def minOperations(n):
     """
-    Calculates the fewest number of operations required to result in exactly n 'H' characters.
-    
+    Calculates the fewest number of operations,
+    required to result in exactly n 'H' characters.
+
     Args:
     n (int): The number of 'H' characters to reach.
 
     Returns:
-    int: The minimum number of operations required to reach exactly n 'H' characters.
+    int: The minimum number of operations required
+    to reach exactly n 'H' characters.
     """
     if n < 2:
         return 0
-    
+ 
     ops = 0
     root = 2
-    
+ 
     while root <= n:
         if n % root == 0:
             ops += root
             n = n / root
             root -= 1
         root += 1
-    
+ 
     return ops

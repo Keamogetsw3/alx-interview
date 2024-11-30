b8e20bc269a22abae0ea238023500f2f12ce1984
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-12-01 00:54:25 +0200

    Update 0-minoperations.py: 0-minoperations.py:22:1: W293 blank line contains whitespace 0-minoperations.py:25:1: W293 blank line contains whitespace 0-minoperations.py:32:1: W293 blank line contains whitespace

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -19,15 +19,15 @@ def minOperations(n):
     """
     if n < 2:
         return 0
- 
+
     ops = 0
     root = 2
- 
+
     while root <= n:
         if n % root == 0:
             ops += root
             n = n / root
             root -= 1
         root += 1
- 
+
     return ops

3f93f1f615880863b8cf0c7c9b0cf436b11eed02
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-12-03 14:08:02 +0200

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-
 import sys
+""" reads stdin line by line and computes metrics """
 
-def display_statistics(status_codes_count, total_size):
+
+def _print(status_dct, total_size):
     """
     Displays the summary of file statistics, including the total file size
     and counts of different status codes.
 
     Args:
-        status_codes_count (dict): A dictionary with status codes as keys (strings)
-                                    and their respective counts as values (integers).
-        total_size (int): The total size of the files processed, in bytes.
+        status_dct: A dictionary with status codes as keys (strings)
+        total_size: The total size of the files processed, in bytes.
 
     Returns:
-        None: This function prints the results to the standard output.
+        None: don’t print anything for this status code
     """
     print("Total file size: {}".format(total_size))
-    for code, count in sorted(status_codes_count.items()):
+    for code, count in sorted(status_dct.items()):
         if count != 0:
             print("{}: {}".format(code, count))
 
 total_size = 0
 status_code = 0
 line_count = 0
-status_codes_count = {
+status_dct = {
     "200": 0,
     "301": 0,
     "400": 0,
@@ -46,12 +46,12 @@ try:
                 total_size += int(parts[0])
                 status_code = parts[1]
 
-                if status_code in status_codes_count:
-                    status_codes_count[status_code] += 1
+                if status_code in status_dct:
+                    status_dct[status_code] += 1
 
             if line_count == 10:
-                display_statistics(status_codes_count, total_size)
+                display_statistics(status_dct, total_size)
                 line_count = 0
 
 finally:
-    display_statistics(status_codes_count, total_size)
+    display_statistics(status_dct, total_size)

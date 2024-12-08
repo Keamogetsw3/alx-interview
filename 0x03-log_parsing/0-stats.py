18ae24be394be04eab16ae39c4adbf590c3b5b79
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2024-12-08 23:28:50 +0200

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python3
+"""
+Write a script that reads stdin line by line and computes metrics:
+"""
 import sys
-"""
-script that reads stdin line by line and computes metrics:
-Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
-(if the format is not this one, the line must be skipped)
-After every 10 lines and/or a keyboard interruption (CTRL + C),
-print these statistics from the beginning:
-Total file size: File size: <total size>
-where <total size> is the sum of all previous <file size> (see input format above)
-Number of lines by status code:
-possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-if a status code doesn’t appear or is not an integer, don’t print anything for this status code
-format: <status code>: <number>
-status codes should be printed in ascending order
-"""
 
 
 line_count = 0
@@ -45,9 +34,10 @@ try:
 
                 for key in sorted_keys:
                     value = status_code[key]
+
                     if value != 0:
                         print('{}: {}'.format(key, value))
-                line_count = 0
+                    line_count = 0
 
 finally:
     print("File size: {:d}".format(total_file_size))
